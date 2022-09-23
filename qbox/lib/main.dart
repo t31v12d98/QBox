24c72e37d6ff706337b7f7267b8fd7974ded261d
t31v12d98 <t31v12d98@gmail.com>
2022-09-23 23:54:43 +0700

    them trang login => testing

--- qbox/lib/main.dart
+++ qbox/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
-import 'MyApp.dart';
+import 'package:qbox/page/login.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(home: Login());
+  }
 }
