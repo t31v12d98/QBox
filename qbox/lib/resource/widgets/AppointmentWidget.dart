f9e7bf9591a9cb9c27c0b781128e967506c9ed5e
NBMinh63343 <100172707+NBMinh63343@users.noreply.github.com>
2022-11-13 17:20:14 +0700

    view

--- qbox/lib/resource/widgets/AppointmentWidget.dart
+++ qbox/lib/resource/widgets/AppointmentWidget.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:qbox/model/slots.dart';
 
 class AppointmentWidgets extends StatefulWidget {
   @override
@@ -12,75 +14,161 @@ class AppointmentWidgets extends StatefulWidget {
 
 class _AppointmentWidgets extends State<AppointmentWidgets> {
   @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      for (int i = 1; i < 5; i++)
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          width: double.infinity,
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(10)),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      InkWell(
-                          onTap: null,
-                          child: CircleAvatar(
-                            backgroundImage: AssetImage('images/1.png'),
-                            minRadius: 30,
-                          )),
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Text(
-                              "Name mentor",
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                            Text("Main Major")
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.green[400],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Text(
-                      "Begin",
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [Text("18 : 00 PM"), Text("30/09/2022")],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: null, child: Text("Enroll"))
-                ],
-              )
-            ],
-          ),
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder<List<Slots>>(
+          stream: readSlots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final slots = snapshot.data!;
+
+              return ListView(
+                children: slots.map(builderSlots).toList(),
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
         ),
-    ]);
-  }
+      );
+  //   for (int i = 1; i < 5; i++)
+  //     Container(
+  //       margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+  //       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+  //       width: double.infinity,
+  //       decoration: BoxDecoration(
+  //           color: Colors.white, borderRadius: BorderRadius.circular(10)),
+  //       child: Column(
+  //         children: [
+  //           Row(
+  //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //             children: [
+  //               Row(
+  //                 children: [
+  //                   InkWell(
+  //                       onTap: null,
+  //                       child: CircleAvatar(
+  //                         backgroundImage: AssetImage('images/1.png'),
+  //                         minRadius: 30,
+  //                       )),
+  //                   Padding(
+  //                     padding: const EdgeInsets.all(10),
+  //                     child: Column(
+  //                       children: [
+  //                         Text(
+  //                           "Name mentor",
+  //                           style: TextStyle(
+  //                               fontSize: 20, fontWeight: FontWeight.bold),
+  //                         ),
+  //                         Text("Main Major")
+  //                       ],
+  //                     ),
+  //                   )
+  //                 ],
+  //               ),
+  //               Container(
+  //                 padding: EdgeInsets.all(5),
+  //                 decoration: BoxDecoration(
+  //                   color: Colors.green[400],
+  //                   borderRadius: BorderRadius.circular(20),
+  //                 ),
+  //                 child: Text(
+  //                   "Begin",
+  //                   style: TextStyle(
+  //                       fontSize: 14,
+  //                       color: Colors.white,
+  //                       fontWeight: FontWeight.bold),
+  //                 ),
+  //               ),
+  //             ],
+  //           ),
+  //           Padding(
+  //             padding: EdgeInsets.only(top: 5),
+  //             child: Row(
+  //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //               children: [Text("18 : 00 PM"), Text("30/09/2022")],
+  //             ),
+  //           ),
+  //           Row(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             children: [
+  //               ElevatedButton(onPressed: null, child: Text("Enroll"))
+  //             ],
+  //           )
+  //         ],
+  //       ),
+  //     ),
+  //
+
+  Stream<List<Slots>> readSlots() => FirebaseFirestore.instance
+      .collection('apointments')
+      .snapshots()
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => Slots.fromJson(doc.data())).toList());
+  Widget builderSlots(Slots slots) => Container(
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        width: 200,
+        height: 100,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    InkWell(
+                        onTap: null,
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('images/1.png'),
+                          minRadius: 30,
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Name mentor",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          Text("Main Major")
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.green[400],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    "Begin",
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text("18 : 00 PM"), Text("30/09/2022")],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: null, child: Text("Enroll"))
+              ],
+            )
+          ],
+        ),
+      );
 }
